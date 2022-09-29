81d5ccba6f73898fb6c1325ca8df21cd48b4d9d5
jescares <jescares@my.centennialcollege.ca>
2022-09-29 14:05:49 -0400

    Initial Commit. (no content yet)

---
+++ COMP229 ASSIGNMENT/routes/index.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res){
+  res.render('index', {
+    title : "Home"
+  })
+});
+
+/* GET About Me page. */
+router.get('/about', function(req, res){
+  res.render('index', {
+    title : "About Me"
+  })
+});
+
+/* GET Projects page. */
+router.get('/projects', function(req, res){
+  res.render('index', {
+    title : "Projects"
+  })
+});
+
+/* GET Services page. */
+router.get('/services', function(req, res){
+  res.render('index', {
+    title : "Services"
+  })
+});
+
+/* GET Contact Me page. */
+router.get('/contact-us', function(req, res){
+  res.render('index', {
+    title : "Contact Me"
+  })
+});
+
+module.exports = router;
