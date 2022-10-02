d3f6359b6bbedc8930a6b2692dd399cb0b6a8682
jescares <jescares@my.centennialcollege.ca>
2022-10-01 22:01:47 -0400

    Second commit: included content for most pages

--- COMP229 ASSIGNMENT/routes/index.js
+++ COMP229 ASSIGNMENT/routes/index.js
@@ -3,21 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res){
-  res.render('index', {
+  res.render('welcome', {
     title : "Home"
   })
 });
 
 /* GET About Me page. */
 router.get('/about', function(req, res){
-  res.render('index', {
+  res.render('about', {
     title : "About Me"
   })
 });
 
 /* GET Projects page. */
 router.get('/projects', function(req, res){
-  res.render('index', {
+  res.render('projects', {
     title : "Projects"
   })
 });
